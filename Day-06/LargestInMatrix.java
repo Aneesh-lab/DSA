f5a0f941de9c236838e6aa031c4e60d3b8e13d2d
Aneesh Mishra <aneeshmishra9098@gmail.com>
2026-07-09 21:11:35 +0530

    2D array and matrix problems

---
+++ Day-06/LargestInMatrix.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class LargestInMatrix {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int[][] matrix = new int[3][3];
+
+        System.out.println("Enter 9 elements:");
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+
+        int largest = matrix[0][0];
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+
+                if (matrix[i][j] > largest) {
+                    largest = matrix[i][j];
+                }
+
+            }
+        }
+
+        System.out.println("Largest Element = " + largest);
+
+        sc.close();
+    }
+}
---
+++ Day-06/SumOfElements.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class SumOfElements {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int[][] arr = new int[3][3];
+
+        int sum = 0;
+
+        for(int i=0;i<3;i++){
+            for(int j=0;j<3;j++){
+                arr[i][j]=sc.nextInt();
+                sum += arr[i][j];
+            }
+        }
+
+        System.out.println("Sum = " + sum);
+        sc.close();
+    }
+}
